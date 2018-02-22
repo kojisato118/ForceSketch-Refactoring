2a0584333cf5ba5b435947756a0291e3a2b17bd6
kojisato118 <k.s.9190118@gmail.com>
2018-02-22 17:41:40 +0900

    とりあえず必要そうなファイル追加

---
+++ ForceSketch-Refactoring/ViewControllers/SketchViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SketchViewController.swift
+//  ForceSketch-Refactoring
+//
+//  Created by 佐藤 康次 on 2018/02/22.
+//  Copyright © 2018年 toosaa. All rights reserved.
+//
+
+import UIKit
+
+class SketchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ForceSketch-Refactoring/Views/SketchView.swift
@@ -0,0 +1,21 @@
+//
+//  SketchView.swift
+//  ForceSketch-Refactoring
+//
+//  Created by 佐藤 康次 on 2018/02/22.
+//  Copyright © 2018年 toosaa. All rights reserved.
+//
+
+import UIKit
+
+class SketchView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
